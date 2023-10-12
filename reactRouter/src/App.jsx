a7be37feea150cb5478fbce4445e9920a76efaba
YatinDevs <c.yatin727@gmail.com>
2023-10-12 19:14:53 +0530

    React_Router_Crash_Course

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-600 p-5">React Router</h1>
+    </>
+  );
+}
+
+export default App;
