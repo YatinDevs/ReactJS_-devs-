9729daef47c7a0ddce443baf1fb288848b93a446
YatinDevs <c.yatin727@gmail.com>
2023-10-28 22:38:57 +0530

    polymorphism

--- JavaScript(ReactBased)/OOPS_in_JS/polymorphism.js
+++ JavaScript(ReactBased)/OOPS_in_JS/polymorphism.js
@@ -24,3 +24,33 @@ const animals = [myDog, myCat];
 for (const animal of animals) {
   animal.speak();
 }
+
+const person = {
+  name: "yatin",
+};
+person.age = 30; // add property
+delete person.name; // delete property
+
+console.log(person);
+
+function Counter() {
+  let count = 0;
+
+  this.increment = function () {
+    count++;
+  };
+  this.decrement = function () {
+    count--;
+  };
+  this.getCount = function () {
+    return count;
+  };
+}
+
+const counter = new Counter();
+counter.increment();
+counter.increment();
+counter.increment();
+
+console.log(counter.count);
+console.log(counter.getCount());
