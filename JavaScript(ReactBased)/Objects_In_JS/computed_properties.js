56d4defbab8f393925fa7744dc852d58e882826f
YatinDevs <c.yatin727@gmail.com>
2023-10-28 09:55:24 +0530

    computed_property_names_featureInJS

---
+++ JavaScript(ReactBased)/Objects_In_JS/computed_properties.js
@@ -0,0 +1,25 @@
+/*
+  What is the computed property in ES6?
+The Computed Property Names Feature.
+The Computed Property Names feature in ES6 allows you to set property names 
+dynamically – that is, property names will be expressions that evaluate to a value.
+
+*/
+
+const key1 = "email";
+const key2 = "password";
+
+const val1 = "c.yatin2323@gmail.com";
+const val2 = "adsdasd231@dad";
+
+const obj = {
+  key1: val1,
+  key2: val2,
+};
+console.log(obj);
+
+const obj2 = {
+  [key1]: val1,
+  [key2]: val2,
+};
+console.log(obj2);
