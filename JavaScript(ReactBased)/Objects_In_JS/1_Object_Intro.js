7de7d07db2b429211a5f29643ac6d9f71f33beec
YatinDevs <c.yatin727@gmail.com>
2023-10-28 10:20:43 +0530

    objects_dot_bracket_notation_computed_Properties_

--- JavaScript(ReactBased)/Objects_In_JS/1_Object_Intro.js
+++ JavaScript(ReactBased)/Objects_In_JS/1_Object_Intro.js
@@ -23,3 +23,26 @@ const person = {
 
 const details = person.getdetails();
 console.log(details);
+// -----------------------------------------------------------
+
+// how to access data from objects (Dot Notation)
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies);
+console.log(person);
+
+// Accessing with help of key other way (Bracket Notation)
+console.log(person["name"]);
+console.log(person["age"]);
+console.log(person["hobbie"]);
+
+// how to add key value pair to objects (Dot Notation)
+person.gender = "male";
+console.log(person);
+
+// adding with help of (bracket Notation)
+
+person["city"] = "kalyan";
+console.log(person);
+
+// -----------------------------------------------------------
