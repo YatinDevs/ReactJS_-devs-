0a7ffbe212d59b645c8bfede4a373d9085eb5522
Yatin Chaudhari <c.yatin727@gmail.com>
2023-11-07 16:49:28 +0530

    Add files via upload

--- React_w_Projects/themeChanger/src/App.jsx
+++ React_w_Projects/themeChanger/src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
+import { ThemeContextProvider } from './contexts/theme';
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
+  const [themeMode,setThemeMode] = useState('light');
+
+  const lightTheme = () => {
+    setThemeMode('light');
+  }
+
+  const darkTheme = () => {
+    setThemeMode('dark');
+  }
+
+  // actual change in theme
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('light','dark');
+    document.querySelector('html').classList.add(themeMode);
+
+  },[themeMode])
 
   return (
-    <>
-      <h1 className='p-4 bg-pink-400 text-3xl'>Welcome</h1>
-    </>
+  <ThemeContextProvider value={{themeMode,lightTheme,darkTheme}}>
+  <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                      {/* themeBtn */}
+                      <ThemeBtn/>
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                      {/* card */}
+                      <Card/>
+
+                    </div>
+                </div>
+  </div>
+  </ThemeContextProvider>
   )
 }
 
